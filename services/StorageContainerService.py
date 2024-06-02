c3f0cc1110932b0383ad2c84f79f34c4f3334eb5
Matheus Weber <matheusweber@microsoft.com>
2024-06-02 13:09:08 -0300

    Addressing comments

--- services/StorageContainerService.py
+++ services/StorageContainerService.py
@@ -12,14 +12,16 @@ class StorageContainerService:
         self.download_container_name = os.getenv('STORAGE_CONTAINER_DOWNLOAD_CONTAINER')
         self.logging = Logger()        
         self.logging.info('SCS-INIT-01 - Running in the serve with Default Azure Credentials')
-        self.blob_service_client = BlobServiceClient(account_url=os.getenv('AZURE_STORAGE_ACCOUNT_URL'), credential=DefaultAzureCredential())
+        self.blob_service_client = BlobServiceClient(account_url=os.getenv('AZURE_STORAGE_ACCOUNT_URL'), 
+                                                     credential=DefaultAzureCredential())
 
     def download_blob(self, blob_name):
         self.logging.info('SCS-DB-01 - Download blob method called')
         try:
             self.logging.info('SCS-DB-02 - Getting blob')
             
-            blob_client = self.blob_service_client.get_blob_client(container=self.download_container_name, blob=blob_name)
+            blob_client = self.blob_service_client.get_blob_client(container=self.download_container_name, 
+                                                                   blob=blob_name)
             if not blob_client.exists():
                 self.logging.error(f"SCS-DB-03 - Blob doenst exists in container.")
                 raise BlobFileDoesntExistsError()
